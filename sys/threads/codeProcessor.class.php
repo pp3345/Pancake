8e7e69369e9355afdb04cdc4f760b48de5886219
Yussuf Khalil <dev@pp3345.net>
2013-01-27 18:56:46 +0100

    Correctly set running flag of CodeProcessor
    Fixes warnings on shutdown when new code was compiled.

--- sys/threads/codeProcessor.class.php
+++ sys/threads/codeProcessor.class.php
@@ -6,12 +6,12 @@
 	/* 2012 Yussuf Khalil                                           */
 	/* License: http://pancakehttp.net/license/                     */
 	/****************************************************************/
-	
+
 	namespace Pancake;
-	
+
 	if(PANCAKE !== true)
 		exit;
-	
+
 	/**
 	 * A code processor runs Moody for Pancake code.
 	 */
@@ -19,7 +19,7 @@
 		public $processFile = "";
 		public $outputFile = "";
 		public $returnThread = 0;
-		
+
 		/**
 		 * Creates a new CodeProcessor
 		 *
@@ -27,20 +27,22 @@
 		 */
 		public function __construct($processFile, $outputFile) {
 			global $Pancake_currentThread;
-			
+
 			$this->codeFile = 'threads/single/codeProcessor.thread.php';
 			$this->friendlyName = 'CodeProcessor';
 			$this->processFile = $processFile;
 			$this->outputFile = $outputFile;
 			$this->returnThread = $Pancake_currentThread && isset($Pancake_currentThread->IPCid) ? $Pancake_currentThread->IPCid : 9999;
 		}
-		
+
 		public function run() {
 			$this->start();
 
 			pcntl_sigprocmask(\SIG_BLOCK, array(\SIGCHLD));
 			pcntl_sigwaitinfo(array(\SIGCHLD));
 			pcntl_wait($x, \WNOHANG);
+
+			$this->running = false;
 
 			if(IPC::get(0, $this->returnThread) !== 1)
 				abort();
