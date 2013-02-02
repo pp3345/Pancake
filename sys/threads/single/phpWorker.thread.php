d84ff36a221d0129cc45c1ddbd08dbbfd318df2d
Yussuf Khalil <dev@pp3345.net>
2013-02-02 21:16:22 +0100

    Fix session cookie

--- sys/threads/single/phpWorker.thread.php
+++ sys/threads/single/phpWorker.thread.php
@@ -400,7 +400,7 @@
 
             #.ifdef 'HAVE_SESSION_EXTENSION'
 	        if(session_id() || vars::$sessionID) {
-	            vars::$Pancake_request->setCookie(session_name(), session_id() ? session_id() : vars::$sessionID, ini_get('session.cookie_lifetime') ? time() + ini_get('session.cookie_lifetime') : 0, ini_get('session.cookie_path'), ini_get('session.cookie_domain'), ini_get('session.cookie_secure'), ini_get('session.cookie_httponly'));
+	            vars::$Pancake_request->setCookie(session_name(), session_id() ? session_id() : vars::$sessionID, ini_get('session.cookie_lifetime') ? time() + ini_get('session.cookie_lifetime') : 0, ini_get('session.cookie_path'), ini_get('session.cookie_domain'), (int) ini_get('session.cookie_secure'), (int) ini_get('session.cookie_httponly'));
 	            session_write_close();
 	            PHPFunctions\sessionID("");
 
