37513a3b4a3ac2b623f0c8d93600d419ba6f6fae
Yussuf Khalil <dev@pp3345.net>
2013-02-12 18:40:10 +0100

    Include debug mode setting in build hash

--- sys/threads/phpWorker.class.php
+++ sys/threads/phpWorker.class.php
@@ -1,19 +1,19 @@
 <?php
-  
+
     /****************************************************************/
     /* Pancake                                                      */
     /* phpWorker.class.php                                          */
     /* 2012 Yussuf Khalil                                           */
     /* License: http://pancakehttp.net/license/                     */
     /****************************************************************/
-    
+
     namespace Pancake;
-    
+
     if(PANCAKE !== true)
         exit;
-    
+
     /**
-    * PHPWorker that runs PHP-scripts    
+    * PHPWorker that runs PHP-scripts
     */
     class PHPWorker extends Thread {
         static private $instances = array();
@@ -23,16 +23,16 @@
         public $socket = null;
         public $socketName = "";
         public $localSocket = null;
-        
+
         /**
-         * 
+         *
          * @var vHost
          */
         public $vHost = null;
-        
+
         /**
         * Creates a new PHPWorker
-        * 
+        *
         * @param vHost $vHost
         * @return PHPWorker
         */
@@ -40,8 +40,8 @@
             $this->vHost = $vHost;
 
             if(!isset(self::$codeProcessed[$vHost->name])) {
-            	$hash = md5(serialize(Config::get('vhosts.' . $vHost->name)) 
-            			. serialize(Config::get('main')) 
+            	$hash = md5(serialize(Config::get('vhosts.' . $vHost->name))
+            			. serialize(Config::get('main'))
             			. serialize((array) Config::get('moody'))
             			. md5_file('vHostInterface.class.php')
             			. md5_file('threads/single/phpWorker.thread.php')
@@ -53,11 +53,12 @@
             			. md5_file('workerFunctions.php')
             			. \PHP_MINOR_VERSION
             			. \PHP_RELEASE_VERSION
-            			. VERSION);
-            	if(!(file_exists('compilecache/phpWorker.thread.' . $vHost->name . '.hash') 
+            			. VERSION
+						. DEBUG_MODE);
+            	if(!(file_exists('compilecache/phpWorker.thread.' . $vHost->name . '.hash')
             	&& file_get_contents('compilecache/phpWorker.thread.' . $vHost->name . '.hash') == $hash)) {
             		require_once 'threads/codeProcessor.class.php';
-            		
+
 	            	$codeProcessor = new CodeProcessor('threads/single/phpWorker.thread.php', 'compilecache/phpWorker.thread.' . $vHost->name . '.cphp');
 	            	$codeProcessor->vHost = $vHost;
 	            	$codeProcessor->run();
@@ -66,17 +67,17 @@
             	self::$codeProcessed[$vHost->name] = true;
             	unset($hash);
             }
-            
+
             // Save instance
             self::$instances[] = $this;
-            
+
             // Set address for IPC based on vHost-ID
             $this->id = max(array_keys(self::$instances));
             $this->IPCid = PHP_WORKER_TYPE.$this->vHost->id;
-            
+
             $this->codeFile = 'compilecache/phpWorker.thread.' . $vHost->name . '.cphp';
             $this->friendlyName = 'PHPWorker #'.($this->id+1).' ("'.$this->vHost->name.'")';
-                        
+
             $this->socketName = Config::get('main.tmppath') . mt_rand() . "_phpworker_local";
             $this->socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
             socket_bind($this->socket, $this->socketName);
@@ -84,7 +85,7 @@
             $this->localSocket = socket_create(AF_UNIX, SOCK_STREAM, 0);
             socket_connect($this->localSocket, $this->socketName);
             $this->socket = socket_accept($this->socket);
-            
+
             // Start worker
             $this->start(false);
         }
--- sys/threads/requestWorker.class.php
+++ sys/threads/requestWorker.class.php
@@ -1,17 +1,17 @@
 <?php
-  
+
     /****************************************************************/
     /* Pancake                                                      */
     /* requestWorker.class.php                                      */
     /* 2012 Yussuf Khalil                                           */
     /* License: http://pancakehttp.net/license/                     */
     /****************************************************************/
-    
+
     namespace Pancake;
-    
+
     if(PANCAKE !== true)
         exit;
-    
+
     /**
     * A request worker processes requests.
     */
@@ -23,21 +23,21 @@
         public $socket = null;
         public $socketName = "";
         public $localSocket = null;
-        
+
         /**
         * Creates a new RequestWorker
-        * 
+        *
         * @return RequestWorker
         */
         public function __construct() {
         	if(!self::$codeProcessed) {
-        		$hash = md5(serialize(Config::get('main')) 
+        		$hash = md5(serialize(Config::get('main'))
         				. serialize((array) Config::get('moody'))
-        				. serialize(Config::get('vhosts')) 
+        				. serialize(Config::get('vhosts'))
         				. serialize(Config::get('fastcgi'))
         				. serialize(Config::get('ajp13'))
         				. md5_file('vHostInterface.class.php')
-        				. md5_file('threads/single/requestWorker.thread.php') 
+        				. md5_file('threads/single/requestWorker.thread.php')
         				. md5_file('HTTPRequest.class.php')
         				. md5_file('invalidHTTPRequest.exception.php')
         				. md5_file('mime.class.php')
@@ -50,7 +50,8 @@
         				. md5_file('AJP13.class.php')
         				. \PHP_MINOR_VERSION
         				. \PHP_RELEASE_VERSION
-        				. VERSION);
+        				. VERSION
+						. DEBUG_MODE);
         		if(!(file_exists('compilecache/requestWorker.thread.hash')
         		&& file_get_contents('compilecache/requestWorker.thread.hash') == $hash)) {
         			require_once 'threads/codeProcessor.class.php';
@@ -62,16 +63,16 @@
         		self::$codeProcessed = true;
         		unset($hash);
         	}
-        	
+
             // Add instance
             self::$instances[] = $this;
-            
+
             // Set id and address for IPC
             $this->id = max(array_keys(self::$instances));
             $this->IPCid = REQUEST_WORKER_TYPE . $this->id;
-            
+
             $this->doGracefulExit = true;
-            
+
             $this->socketName = Config::get('main.tmppath') . mt_rand() . "_rworker_local";
             $this->socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
             socket_bind($this->socket, $this->socketName);
@@ -79,7 +80,7 @@
             $this->localSocket = socket_create(AF_UNIX, SOCK_STREAM, 0);
             socket_connect($this->localSocket, $this->socketName);
             $this->socket = socket_accept($this->socket);
-            
+
             $this->codeFile = 'compilecache/requestWorker.thread.cphp';
             $this->friendlyName = 'RequestWorker #' . ($this->id + 1);
         }
